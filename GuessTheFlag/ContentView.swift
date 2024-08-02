d6e8dd4846259ce9f9a5a6f44c8fecd5ab028218
chef-nic <nicholasajohnson@me.com>
2024-08-02 14:11:46 -0700

    added accessibility for voiceover users

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -16,6 +16,20 @@ struct ContentView: View {
     @State private var questionsShown = 0
     @State private var showingGameOver = false
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     var body: some View {
         ZStack {
             RadialGradient(stops: [
@@ -53,6 +67,7 @@ struct ContentView: View {
                         flagTapped(number)
                     } label: {
                         FlagImage(country: countries[number])
+                            .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                     }
                 }
             }
